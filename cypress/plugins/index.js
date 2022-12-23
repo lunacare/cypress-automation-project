5a3e83325754cdfa3b9ed1008af4a778a99bbc0c
Juan <juan.delossantos@koombea.com>
2022-12-23 16:40:10 -0400

    Initial commit

---
+++ cypress/plugins/index.js
@@ -0,0 +1,63 @@
+/// <reference types="cypress" />
+const {GoogleSocialLogin} = require('cypress-social-logins').plugins
+
+// ***********************************************************
+// This example plugins/index.js can be used to load plugins
+//
+// You can change the location of this file or turn off loading
+// the plugins file with the 'pluginsFile' configuration option.
+//
+// You can read more here:
+// https://on.cypress.io/plugins-guide
+// ***********************************************************
+
+// This function is called when a project is opened or re-opened (e.g. due to
+// the project's config changing)
+async function fewMoreSteps({page, options} = {}) {
+
+  //await page.waitForSelector("input[type='tel']")
+
+  cy.task("generateOTP", "3f7data27rlplumdbkz3vkrfwftvlvc3").then(token => {
+    cy.get("input[type='tel']").type(token)
+
+  cy.get('#totpNext > div > button').click()
+
+  cy.wait(15000)
+  })
+  // ... define steps
+}
+/**
+ * @type {Cypress.PluginConfig}
+ */
+// eslint-disable-next-line no-unused-vars
+async function moreSteps({page, options} = {}) { 
+  cy.task("generateOTP", "3f7data27rlplumdbkz3vkrfwftvlvc3").then(token => {
+    cy.get("input[type='tel']").type(token)
+
+  cy.get('#totpNext > div > button').click()
+
+  cy.wait(15000)
+  })
+  
+  
+  //await page.click('#pin_Field') 
+}
+
+
+module.exports = (on, config) => {
+
+  let shared_vars = {}
+
+  on('task', {
+    GoogleSocialLogin: GoogleSocialLogin,
+    generateOTP: require("cypress-otp"),
+    saveSharedVar(variable){
+      shared_vars[variable.name] = variable.value
+      return null
+    },
+    getSharedVars(){
+      return shared_vars || {}
+    }
+  })
+}
+
